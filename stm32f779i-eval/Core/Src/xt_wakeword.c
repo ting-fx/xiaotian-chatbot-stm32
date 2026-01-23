5e6694597489e58eb231c6ce06942e6d48b26cc0
Ting <zhuting100@163.com>
2026-01-23 17:18:13 +0800

    Update comments.

--- stm32f779i-eval/Core/Src/xt_wakeword.c
+++ stm32f779i-eval/Core/Src/xt_wakeword.c
@@ -41,20 +41,23 @@ uint8_t wakeword_detection(){
 
     ULONG actual_events;
 
+    /* 通知GUI线程录音开始，更新界面显示 */
     tx_event_flags_set(&xt_event_group, XT_EVENT_GUI_RECORDING, TX_OR);
 
     while (1)
     {
+    	/* 获取录音准备就绪标志 */
     	tx_event_flags_get(&xt_event_group, XT_EVENT_AUDIO_MASK, TX_OR_CLEAR , &actual_events, TX_WAIT_FOREVER);
 
     	if(actual_events & XT_EVENT_AUDIO_WAKEWORD_READY)
     	{
     		AUDIO_DATA audio_data;
 
+    		/* 获取录音数据 */
 			if (audio_record_data_get(&audio_data) == STATUS_SUCCESS) {
 				int32_t keyword_index;
 
-				// Detect wake word.
+				/* 开始检测唤醒词 */
 				const pv_status_t is_wakeword = pv_porcupine_process(handle, (int16_t *)audio_data.data_ptr, &keyword_index);
 				if((is_wakeword == PV_STATUS_SUCCESS) && (keyword_index != -1))
 				{
@@ -65,6 +68,7 @@ uint8_t wakeword_detection(){
     	}
     }
 
+    /* 通知GUI线程检测到唤醒词，更新界面显示 */
     tx_event_flags_set(&xt_event_group, XT_EVENT_GUI_DETECTED, TX_OR);
 
     pv_porcupine_delete(handle);
